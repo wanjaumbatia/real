a5b39a99f41866eabeda858c39ed7a680d3c8bb2
Francis Mbatia <francis.mbatia@gpsgc.com>
2022-09-07 07:48:39 +0000

    ui fix navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,11 +35,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        @if(Auth::user()->operations_manager==true)
                         <li class="nav-item">
                             <a class="nav-link active" href="{{ route('admin.team.list') }}">{{ __('Users') }}</a>
                         </li>
-                        @endif
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('admin.targets.list') }}">{{ __('Targets') }}</a>
                         </li>
