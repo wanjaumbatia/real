4ed2ff7693040ea45fd33a2664f28be6f90c74ab
Francis Mbatia <francis.mbatia@gpsgc.com>
2022-09-28 04:13:13 +0000

    .

--- resources/views/layouts/sales.blade.php
+++ resources/views/layouts/sales.blade.php
@@ -21,7 +21,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.sstatic.net/Sites/stackoverflow/primary.css?v=1a040064279b">
+
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 </head>
