6ab0bd2e7f4a9292b560146c4a9f968dc0e4d2f4
Francis Mbatia <francis.mbatia@gpsgc.com>
2022-09-29 06:11:20 +0000

    .

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -128,7 +128,7 @@ class OfficeController extends Controller
             'customer_id' => $customer->id,
             'customer_number' => $customer->no,
             'plans_id' => $plan->id,
-            'name' => 'Regular',
+            'name' => $plan->id,
             'created_by' => "Admin",
             'active' => true,
             'branch' => $customer->branch,
