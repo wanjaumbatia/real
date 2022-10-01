323072199c2754a860cc05495827472d97da287e
Francis Mbatia <francis.mbatia@gpsgc.com>
2022-10-01 13:06:22 +0000

    .

--- resources/views/sales/customer.blade.php
+++ resources/views/sales/customer.blade.php
@@ -94,10 +94,30 @@
                         <div class="card-body">
                             <a href="/loan/{{$customer->id}}" class="btn btn-primary btn-sm w-100 my-1">Apply Loan</a>
                             <a href="/collection/{{$customer->id}}" class="btn btn-primary btn-sm w-100 my-1">Go to payments</a>
-                            <button  class="btn btn-primary btn-sm w-100 my-1">New Plan</buttin>
+                            <button data-target="#modal" class="btn btn-primary btn-sm w-100 my-1">New Plan</buttin>
                         </div>
                     </div>
-                    
+
+                    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
